6e8f90a4d8abbb5700abc39eb10026ac047827b4
inFlorence <inFlorence@gmail.com>
2022-06-12 19:56:30 +0900

    Update complex_numbering.py
    - solve with BFS

--- complex_numbering.py
+++ complex_numbering.py
@@ -1,52 +1,75 @@
-# BAKEJOON-2667 
-# https://www.acmicpc.net/problem/2667
-
 import sys
-
+from collections import deque
+ 
+ 
 def Input_Data():
-	readl = sys.stdin.readline
-	N = int(readl())
-	map_apt = [list(map(int,list(readl().strip()))) for r in range(N)]
-	return N, map_apt   
-
-size = 0
-d = ((-1, 0), (1, 0), (0, -1), (0, 1))
-
+    readl = sys.stdin.readline
+    N = int(readl())
+    map_apt = [[0] + list(map(int,list(readl().strip()))) + [0] if 1<=r<=N else [0]*(N+2) for r in range(N+2)]
+    return N, map_apt
+ 
+ 
+sys.setrecursionlimit(50 * 50) # Recursion 중첩 제한 설정
 def Flood_Fill_DFS(r, c):
     global size
-    map_apt[r][c] = 0
     size += 1
-    for dr, dc in d:
-        nr, nc = r + dr, c + dc
-        if not 0 <= nr < N : continue
-        if not 0 <= nc < N : continue
+    map_apt[r][c] = 0
+ 
+    for dr,dc in d:
+        nr,nc = r+dr,c+dc
+        if not 1 <= nr <= N: continue
+        if not 1 <= nc <= N: continue
         if map_apt[nr][nc] == 0: continue
         Flood_Fill_DFS(nr, nc)
-    
+ 
+ 
+def Flood_Fill_BFS(r, c):
+    global q
+    q = deque()
+    map_apt[r][c] = 0
+    q.append((r, c))
+    size = 1
+    while q:
+        r,c = q.popleft()
+        for dr,dc in d:
+            nr, nc = r+dr, c+dc
+            if not 1 <= nr <= N: continue
+            if not 1 <= nc <= N: continue
+            if map_apt[nr][nc] == 0 : continue
+            map_apt[nr][nc] = 0
+            q.append((nr, nc))
+            size += 1
+    return size
+ 
+ 
 def Solve():
     global size
     list_size = []
-    sys.setrecursionlimit(N*N)
-    # 셀 각각 스캔 진행 
-    for r in range(N): 
-        for c in range(N):
-            if map_apt[r][c] == 0: continue
-            size = 0 # 재귀함수에서 size 를 증가시킴 
-            Flood_Fill_DFS(r, c)
-            list_size.append(size) # 해당 단지의 집의 개수
+    for r in range(1,N+1):
+        for c in range(1,N+1):
+            if map_apt[r][c]:
+                """
+                # DFS 방식 Flood-Fill
+                size = 0
+                Flood_Fill_DFS(r, c) """
+ 
+                # BFS 방식 Flood-Fill
+                size = Flood_Fill_BFS(r, c)
+                 
+                list_size.append(size)
     list_size.sort()
-    return list_size  
-            
-cnt = -1
-list_size = []
-
+    return len(list_size), list_size
+ 
+ 
 # 입력 받는 부분
 N, map_apt = Input_Data()
-
+ 
 # 여기서부터 작성
-list_size = Solve()
-cnt = len(list_size)
-
+size = 0
+d = ((1,0),(-1,0),(0,1),(0,-1))
+q = 0
+cnt,list_size = Solve()
+ 
 # 출력하는 부분
 print(cnt)
 print(*list_size,sep='\n')
